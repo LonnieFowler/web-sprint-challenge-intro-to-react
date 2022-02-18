706945bb90aab801d9b6d8ff47909585c88310a9
LonnieFowler <97270531+LonnieFowler@users.noreply.github.com>
2022-02-18 15:33:03 -0700

    sprint challenge complete?

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
+
 import './App.css';
+import Character from './components/Character';
+import styled from 'styled-components';
+
+const Header1 = styled.h1`
+    color: red;
+    font-size: 100px;
+    font-family: cursive;
+`;
 
 const App = () => {
+
+ const [peeps,setPeeps] = useState()
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then(res =>{
+      setPeeps(res.data)
+    })
+    .catch(err => console.log(err))
+  },[])
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <Header1 className="Header">Characters</Header1>
+      {peeps && peeps.map((el) => (
+        <Character key={el.name} name={el.name} birth_year={el.birth_year} films={el.films}/>
+      ))}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,60 @@
 // Write your Character component here
+import react from "react"
+import styled from "styled-components"
+
+const BrightDiv = styled.div`
+    background-color: lemonchiffon;
+    width: 50%;
+    margin-left: 25%;
+`;
+const BirthYear = styled.p`
+    color: blue;
+`;
+const Films = styled.p`
+    color: green;
+`;
+const Header = styled.h2`
+    color: orange;
+`;
+
+
+function Character (props){
+    return (
+        <BrightDiv className='Character-wrapper'>
+            <Header>{props.name}</Header>
+            <BirthYear>{props.birth_year}</BirthYear>
+            {props.films.map((el) => (
+                <Films>{el}</Films>
+            ))}
+        </BrightDiv>
+    )
+}
+
+
+/*birth_year: "19BBY"
+created: "2014-12-09T13:50:51.644000Z"
+edited: "2014-12-20T21:17:56.891000Z"
+eye_color: "blue"
+films: (4) ['A New Hope', 'The Empire Strikes Back', 'Return of the Jedi', 'Revenge of the Sith']
+gender: "male"
+hair_color: "blond"
+height: "172"
+homeworld: "http://swapi.dev/api/planets/1/"
+mass: "77"
+name: "Luke Skywalker"
+skin_color: "fair"
+species: Array(0)
+length: 0
+[[Prototype]]: Array(0)
+starships: Array(2)
+0: "http://swapi.dev/api/starships/12/"
+1: "http://swapi.dev/api/starships/22/"
+length: 2
+[[Prototype]]: Array(0)
+url: "http://swapi.dev/api/people/1/"
+vehicles: (2) ['http://swapi.dev/api/vehicles/14/', 'http://swapi.dev/api/vehicles/30/'
+*/
+
+
+
+export default Character
